9b22d2b221c0c1c717a068bc051a63d62fe5da94
MauricioCornejo <mauricio.cornejo.castro@gmail.com>
2020-06-15 17:37:28 -0400

    comitsea

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ const config = {
   port: process.env.PORT || 3000,
   host: process.env.HOST || 'http://localhost',
   publicRoute: process.env.PUBLIC_ROUTE || '/app' ,
+  algo: "algo",
   filesRoute: process.env.FILES_ROUTE || 'files'
 }
 
